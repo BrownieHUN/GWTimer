1f946ed9cc7707e2019cc430191ca5109532d44b
BrownieHUN <92264890+BrownieHUN@users.noreply.github.com>
2022-09-26 21:53:05 +0200

    Update

--- javascript.js
+++ javascript.js
@@ -1,22 +1,46 @@
 var txt;
 var r = document.querySelector(':root');
+var starth = [];
+var startm = [];
+var endh = [];
+var endm = [];
+var dur = [];
+var duration = 0;
+var loc = [];
+var other = [];
+const d = new Date();
 
-function readFileAutomatically () {
+function readFileAutomatically() {
         var client = new XMLHttpRequest();
         client.open('GET', 'https://raw.githubusercontent.com/BrownieHUN/GWTimer/main/data.txt');
         client.onreadystatechange = function() {
             if( client.responseText != '' ) {
-                var txt = client.responseText.split("\n");
-                
-                document.getElementById("k1").innerHTML = txt[0] + '<b><sup>' + txt[1] + '</sup></b>';
-                document.getElementById("v1").innerHTML = txt[2] + '<b><sup>' + txt[3] + '</sup></b>';
-                document.getElementById("h1").innerHTML = txt[4];
-                document.getElementById("e1").innerHTML = txt[6];
+                txt = client.responseText.split("\n");
+                for(i = 0; i < txt.length / 7; i++) {
+                    starth[i] = parseInt(txt[i * 7]);
+                    startm[i] = parseInt(txt[i * 7 + 1]);
+                    endh[i] = parseInt(txt[i * 7 + 2]);
+                    endm[i] = parseInt(txt[i * 7 + 3]);
+                    dur[i] = parseInt(txt[i * 7 + 4]);
+                    loc[i] = txt[i * 7 + 5];
+                    other[i] = txt[i * 7 + 6];
+                }
             }
         }
         client.send();
     }
-function delay(n){
+
+function teszt() {
+    if(starth[0] != endh[0]) {
+        duration = 60 - startm[0] + (endh[0] - starth[0] - 1) * 60 + endm[0];
+    }
+    else {
+        duration = endm - startm;
+    }
+    console.log(duration)
+}
+
+function delay(n) {
     return new Promise(function(resolve){
         setTimeout(resolve,n*1000);
     });
