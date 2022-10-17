884f10fc02207a43ba812be572783d18123b4259
BrownieHUN <92264890+BrownieHUN@users.noreply.github.com>
2022-10-17 20:25:05 +0200

    Update

--- javascript.js
+++ javascript.js
@@ -13,7 +13,8 @@ const d = new Date();
 
 function readFile() {
     var client = new XMLHttpRequest();
-	client.open('GET', '/assets/data' + d.getDay() + '.txt');
+	//client.open('GET', '/static/data' + d.getDay() + '.txt');
+    client.open('GET', 'https://i.gonda.dev/76si1rm4ildt.txt')
     client.onreadystatechange = function() {
         if( client.responseText != '' ) {
             txt = client.responseText.split("\n");
@@ -34,25 +35,38 @@ function readFile() {
 	client.send();
 }
 
-timer = false;
+async function secClock() {
+    const sec_time = new Date();
+    document.getElementById("sec").style.transform = "rotate(" + sec_time.getSeconds() * 6 + "deg)";
+    setTimeout(secClock, 1000);
+}
 
-/*async function secClock() {
-    time = d.getSeconds();
-    if(timer == false) {
-        document.getElementById("sec").style.visibility = "visible";
-        timer = true;
-        while(timer == true) {
-            document.getElementById("sec").style.transform = "rotate(" + time * 6 + "deg)";
-            time++;
-            await delay(1);
-        }
-    }
-    else {
-        document.getElementById("sec").style.visibility = "hidden";
-        timer = false;
-    }
+async function clock() {
+    const clock_time = new Date();
+    cur_min = clock_time.getMinutes();
+    cur_hrs = clock_time.getHours();
 
-}*/
+    document.getElementById("time").innerHTML = cur_hrs + ":" + cur_min;
+    setTimeout(clock, 1000);    
+}
+
+function insertTable() {
+    cell = [];
+    ctable = document.createElement("TABLE");
+    ctable.setAttribute("id", "tcontent");
+    document.body.appendChild(ctable);
+
+	for(i = 0; i < txt.length / 6; i++) {  
+		row = ctable.insertRow(i);
+		for(j = 0; j < 4; j++) {
+			cell[j+1] = row.insertCell(j);
+		}
+        cell[1].innerHTML = starth[i] + "<b><sup>" + startm[i] + "</sup></b>";
+        cell[2].innerHTML = endh[i] + "<b><sup>" + endm[i] + "</sup></b>";
+        cell[3].innerHTML = loc[i];
+        cell[4].innerHTML = other[i];
+	}
+}
 
 var n = 0;
 var tlen = [];
@@ -63,34 +77,36 @@ var cur_pause = false;
 async function start() {
     //az első időponttól eltelt idő (másodpercek)
     var elapsed = (starth[0] != d.getHours()) ? (60 - startm[0] + (d.getHours() - starth[0] - 1) * 60 + d.getMinutes()) * 60 + d.getSeconds() : (d.getMinutes() - startm[0]) * 60 + d.getSeconds();
+
     //mínusz, ha még nem indult el
-    for(i = 0; i < txt.length / 6; i++) {
-        //minden óra és az utána következő szünet hossza (már ha van), plusz az előzők
-        tlen[i] = (i > 0) ? (i != txt.length / 6 - 1) ? tlen[i-1] + (dur[i] + pause[i]) * 60 : tlen[i-1] + dur[i] * 60 : (dur[i] + pause[i]) * 60;
-        if(tlen[i] < elapsed) { continue; }
-        else {
-            n = i;
-            //ha van még maradék letelni való óra
-            if(elapsed < tlen[i] - pause[i] * 60) {
-                //r.style.setProperty('--circlecolor', 'var(--activecolor)');
-                //deg_elapsed = 2 * 760 * Math.PI * (elapsed - tlen[i-1]) / (tlen[i] - tlen[i-1]);
-                deg_elapsed = (tlen[i-1]) ? 2 * 760 * Math.PI * ((elapsed - tlen[i-1]) / (dur[i] * 60)) : 2 * 760 * Math.PI * (elapsed / (dur[i] * 60));
-                r.style.setProperty('--startdeg', deg_elapsed);
-                //vonja ki az eltelt időt az aktuális kezdetéből ,ezzel megkapva, hogy mennyi telt el, majd ossza el az aktuális rész hosszával
-                t_remain = (pause[i]) ? tlen[i] - elapsed - pause[i] * 60 : tlen[i] - elapsed;
-                //r.style.animation = "ractive " + t_remain + "s 1 linear";
-                cur_pause = false;
-            }
-            else {
-                //r.style.setProperty('--circlecolor', 'var(--inactivecolor)');
-                deg_elapsed = (tlen[i-1]) ? 2 * 760 * Math.PI * ((elapsed - tlen[i-1] - dur[i] * 60) / (pause[i] * 60)) : 2 * 760 * Math.PI * ((elapsed - dur[i] * 60) / (pause[i] * 60));
-                r.style.setProperty('--startdeg', deg_elapsed);
-                t_remain = tlen[i] - elapsed;
-                //r.style.animation = "rinactive " + t_remain + "s 1 linear";
-                cur_pause = true;
-            }
-        }
-        break;
+    if(elapsed < 0) {
+        t_remain = starth[0] * 60 * 60 + startm[0] * 60 - d.getHours() * 60 * 60 - d.getMinutes() * 60 - d.getSeconds();
+        cur_pause = true;
+    }
+    else {
+		for(i = 0; i < txt.length / 6; i++) {
+			//minden óra és az utána következő szünet hossza (már ha van), plusz az előzők
+			tlen[i] = (i > 0) ? (i != txt.length / 6 - 1) ? tlen[i-1] + (dur[i] + pause[i]) * 60 : tlen[i-1] + dur[i] * 60 : (dur[i] + pause[i]) * 60;
+			if(tlen[i] < elapsed) { n++; continue; }
+			else {
+				n = i;
+				//ha van még maradék letelni való óra
+				if(elapsed < tlen[i] - pause[i] * 60) {
+					deg_elapsed = (tlen[i-1]) ? 2 * 760 * Math.PI * ((elapsed - tlen[i-1]) / (dur[i] * 60)) : 2 * 760 * Math.PI * (elapsed / (dur[i] * 60));
+					r.style.setProperty('--startdeg', deg_elapsed);
+					//vonja ki az eltelt időt az aktuális kezdetéből ,ezzel megkapva, hogy mennyi telt el, majd ossza el az aktuális rész hosszával
+					t_remain = (pause[i]) ? tlen[i] - elapsed - pause[i] * 60 : tlen[i] - elapsed;
+					cur_pause = false;
+				}
+				else {
+					deg_elapsed = (tlen[i-1]) ? 2 * 760 * Math.PI * ((elapsed - tlen[i-1] - dur[i] * 60) / (pause[i] * 60)) : 2 * 760 * Math.PI * ((elapsed - dur[i] * 60) / (pause[i] * 60));
+					r.style.setProperty('--startdeg', deg_elapsed);
+					t_remain = tlen[i] - elapsed;
+					cur_pause = true;
+				}
+			}
+			break;
+		}
     }
 }
 
@@ -102,7 +118,7 @@ function writeData() {
         document.getElementById("e1").innerHTML = other[i];
     }
     else {
-    	document.getElementById("k1").innerHTML = endh[i] + "<b><sup>" + endm[i] + "</sup></b>";
+        document.getElementById("k1").innerHTML = (endh[i]) ? endh[i] + "<b><sup>" + endm[i] + "</sup></b>" : "";
     	document.getElementById("v1").innerHTML = (starth[i+1]) ? starth[i+1] + "<b><sup>" + startm[i+1] + "</sup></b>" : "";
     	document.getElementById("h1").innerHTML = (loc[i+1]) ? "→ " + loc[i+1] : "";
     	document.getElementById("e1").innerHTML = "";
@@ -115,11 +131,7 @@ function writeData() {
 
 async function play() {
     start();
-<<<<<<< Updated upstream
 	for(i = n; i <= txt.length / 6; i++) {
-=======
-	for(i = n; i < txt.length / 6; i++) {
->>>>>>> Stashed changes
         if(cur_pause == false) {
             r.style.setProperty('--circlecolor', 'var(--activecolor)');
             writeData();
@@ -131,7 +143,6 @@ async function play() {
             cur_pause = !cur_pause;
             r.style.setProperty('--startdeg', 0);
         }
-<<<<<<< Updated upstream
 
         if(cur_pause == true && i+1 != txt.length / 6) {
             if(pause[i] != 0) {
@@ -142,15 +153,6 @@ async function play() {
                 await delay(t_remain);
     		    //await delay(2);
             }
-=======
-        if(cur_pause == true && i+1 != txt.length / 6) {
-    		r.style.setProperty('--circlecolor', 'var(--inactivecolor)');
-    		r.style.animation = "rinactive " + t_remain + "s 1 linear";
-    		//r.style.animation = "rinactive 2s 1 linear";
-    		writeData();
-            await delay(t_remain);
-		    //await delay(2);
->>>>>>> Stashed changes
             t_remain = dur[i+1] * 60;
             cur_pause = !cur_pause;
             r.style.setProperty('--startdeg', 0);
@@ -162,4 +164,12 @@ function delay(n) {
     return new Promise(function(resolve){
         setTimeout(resolve,n*1000);
     });
+}
+
+async function run() {
+    readFile();
+    await delay(0.5);
+    insertTable();
+/*    await delay(0.3);
+    play();*/
 }
